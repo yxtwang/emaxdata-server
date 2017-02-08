5f54fe93ebc5dd881f0390406442eb9f957667ca
yxtwang <yxtwang@gmail.com>
2017-02-08 14:23:43 +0800

    Initial commit

---
+++ src/main/java/com/elinkzine/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.elinkzine.config.audit;
---
+++ src/main/java/com/elinkzine/repository/PersistentTokenRepository.java
@@ -0,0 +1,19 @@
+package com.elinkzine.repository;
+
+import com.elinkzine.domain.PersistentToken;
+import com.elinkzine.domain.User;
+import java.time.LocalDate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the PersistentToken entity.
+ */
+public interface PersistentTokenRepository extends JpaRepository<PersistentToken, String> {
+
+    List<PersistentToken> findByUser(User user);
+
+    List<PersistentToken> findByTokenDateBefore(LocalDate localDate);
+
+}
